429548971eb655c6079e4bf077ef2d34e6556d06
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-10-13 06:30:15 -0400

    Made changes to the server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas'); // Import schemas
+const { authMiddleware } = require('./utils/auth'); // Import authentication middleware
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,13 +11,25 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// Create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+// Apply the Apollo GraphQL middleware and set the path to /graphql
+server.applyMiddleware({ app, path: '/graphql' });
+
+// Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
+
