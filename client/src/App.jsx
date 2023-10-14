75510b73ea69f6a19830ba1a21067016ea510c43
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-10-14 07:23:51 -0400

    Created queries.js file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,27 @@
+import React from 'react';
 import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { Outlet } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
+
+// Setup Apollo Client
+const uri = '/graphql';
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  uri,
+  cache,
+  headers: {
+    authorization: localStorage.getItem('id_token') || '',
+  },
+});
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
