fc6a1d3030d96c71076465a27ac2b251379ac7aa
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-10-14 09:11:39 -0400

    Created .env file to store sensitive information for the MongoDB connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,10 @@
+require('dotenv').config(); // Load environment variables from .env file
+
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+const MONGODB_URI = process.env.MONGODB_URI;
 
-module.exports = mongoose.connection;
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
